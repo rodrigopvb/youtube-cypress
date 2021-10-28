e4d0d86955d9484089539e758aa474fbebae1031
rodrigoboas <rodrigopaixaovb@gmail.com>
2021-10-28 19:00:05 -0300

    update

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,22 +23,3 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
-
-Cypress.Commands.add("createOng", () => {
-    cy.request({
-        method: 'POST',
-        url: 'http://localhost:3333/ongs',
-        body: {
-            name: "Gatos queridos",
-            email: "gatos@mail.com",
-            whatsapp: "519999999999",
-            city: "Porto Alegre",
-            uf: "RS"    
-        }
-    }).then(response => {
-        expect(response.body.id).is.not.null;
-        cy.log(response.body.id);
-
-        Cypress.env('createdOngId', response.body.id);
-    });
-})
